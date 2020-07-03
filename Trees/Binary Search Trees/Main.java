5a21e709be0e1f07bd530a89fb09beac0ff72849
Akbar Habib <akbarhasnani3@gmail.com>
2020-07-03 16:18:55 -0500

    <Added implementation for traversing in order and searching and getting a value for binary search trees>

---
+++ Trees/Binary Search Trees/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tree intTree = new Tree();
+
+        intTree.insertTreeNode(15);
+        intTree.insertTreeNode(25);
+        intTree.insertTreeNode(10);
+        intTree.insertTreeNode(5);
+        intTree.insertTreeNode(35);
+
+        // intTree.traverseInOrder();
+
+        System.out.println(intTree.get(10).getData());
+    }
+}
---
+++ Trees/Binary Search Trees/Tree.java
@@ -0,0 +1,29 @@
+public class Tree {
+
+    private TreeNode root;
+
+    public void insertTreeNode(int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+        }
+
+        else {
+            root.insert(value);
+        }
+    }
+
+    public void traverseInOrder() {
+        if (root != null) {
+            root.traverseInOrder();
+        }
+    }
+
+    public TreeNode get(int value) {
+        if (root != null) {
+            return root.get(value);
+        }
+
+        return null;
+    }
+
+}
